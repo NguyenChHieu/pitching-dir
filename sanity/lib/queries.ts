d0d86f0523b6dbc359bb8cc1c881b882086ff0a6
NguyenChHieu <nguyenchihieu178205@gmail.com>
2025-02-19 22:21:22 +1100

    filter the posts by search.

--- sanity/lib/queries.ts
+++ sanity/lib/queries.ts
@@ -1,7 +1,7 @@
 import { defineQuery } from "next-sanity";
 
 export const STARTUPS_QUERY = defineQuery(
-    `*[_type == "startup" && defined(slug.current)] | order(_createdAt desc) {
+  `*[_type == "startup" && defined(slug.current) && !defined($search) || title match $search || category match $search || author->name match $search] | order(_createdAt desc) {
         _id,
         title,
         slug,
@@ -17,4 +17,4 @@ export const STARTUPS_QUERY = defineQuery(
             bio
         }
     }`
-)
+);
